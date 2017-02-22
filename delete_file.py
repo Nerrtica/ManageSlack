0c40f6145ae1d3ad163058a174b4020c1684ad84
Nerrtica <nerrtica@naver.com>
2017-02-22 21:24:26 +0900

    delete_file.py class화

--- delete_file.py
+++ delete_file.py
@@ -1,109 +1,89 @@
-#-*- coding:utf-8 -*-
-
 import requests
 import time
 import json
 
-# get your token from here -> https://api.slack.com/docs/oauth-test-tokens
-token = ''
-# delete files uploaded before n days
-before_n_days = 1
-# file_type = all / spaces / snippets / images / videos / audios / gdocs / zips / pdfs
-file_type = 'all'
-exclude_starred_items = True
 
-def get_my_id_nick():
-    params = {
-        'token': token
-    }
-    uri = 'https://slack.com/api/auth.test'
-    response = requests.get(uri, params=params)
-    text = json.loads(response.text)
-    return (text['user_id'], text['user'])
+class DeleteFile:
 
-def list_files(before_n_days=30, user_id='', file_type='images', exclude_starred_items=True):
-    """
-    :param before_n_days: n일 이전에 올라온 파일만 받아옴. default=30
-    :param user_id: 특정 유저가 업로드한 파일만 받아옴. 빈 문자열일 경우 모든 유저의 파일을 받아옴. default=''
-    :param file_type: 특정 타입의 파일만 받아옴. default='images'
-                      * 'all': All files
-                      * 'spaces: Posts
-                      * 'snippets': Snippets
-                      * 'images': Image files
-                      * 'videos': Video files
-                      * 'audios': Audio files
-                      * 'gdocs': Google docs
-                      * 'zips': Zip files
-                      * 'pdfs': PDF files
-    :param exclude_starred_items: 유저에게 Star되어있는 파일을 제외할지의 옵션. default=True
-    """
-    types = file_type
-    if file_type == 'videos' or file_type == 'audios':
-        types = 'all'
-    
-    params = {
-        'token': token,
-        'ts_to': int(time.time()) - before_n_days * 24 * 60 * 60,
-        'ts_from': 0,
-        'count': 1000,
-        'types': types,
-    }
-    if user_id != '':
-        params['user'] = user_id
-        
-    uri = 'https://slack.com/api/files.list'
-    response = requests.get(uri, params=params)
-    pages_num = json.loads(response.text)['paging']['pages']
-    
-    result_files = []
-    for i in range(1, pages_num+1):
-        params['page'] = i
+    def __init__(self, token, before_n_days=1, file_type='images', exclude_starred_items=True):
+        self.token = token
+        self.before_n_days = before_n_days
+        self.file_type = file_type
+        self.exclude_starred_items = exclude_starred_items
+
+    def get_my_id_nick(self):
+        params = {
+            'token': self.token
+        }
+        uri = 'https://slack.com/api/auth.test'
         response = requests.get(uri, params=params)
-        files = json.loads(response.text)['files']
-        
-        for file in files:
-            # Starred items 제외
-            if exclude_starred_items:
-                try:
-                    is_starred = file['is_starred']
-                    if is_starred: continue
-                except KeyError:
-                    pass
+        text = json.loads(response.text)
+        return text['user_id'], text['user']
 
-            # Pinned items 제외
-            try:
-                pinned_to = file['pinned_to']
-                if len(pinned_to) > 0: continue
-            except KeyError:
-                pass
-            
-            # Video & Audio files 수동 
-            if file_type == 'videos' or file_type == 'audios':
-                if file['mimetype'].split('/')[0] != file_type[:-1]:
+    def list_files(self):
+        userid, nickname = self.get_my_id_nick()
+        types = self.file_type
+        if self.file_type == 'videos' or self.file_type == 'audios':
+            types = 'all'
+
+        params = {
+            'token': self.token,
+            'ts_to': int(time.time()) - self.before_n_days * 24 * 60 * 60,
+            'ts_from': 0,
+            'count': 1000,
+            'types': types,
+        }
+        if userid != '':
+            params['user'] = userid
+
+        uri = 'https://slack.com/api/files.list'
+        response = requests.get(uri, params=params)
+        pages_num = json.loads(response.text)['paging']['pages']
+
+        result_files = []
+        for i in range(1, pages_num + 1):
+            params['page'] = i
+            response = requests.get(uri, params=params)
+            files = json.loads(response.text)['files']
+
+            for file in files:
+                # Starred items 제외
+                if self.exclude_starred_items:
+                    if file.get('is_starred', False):
+                        continue
+
+                # Pinned items 제외
+                if len(file.get('pinned_to', [])) > 0:
                     continue
 
-            result_files.append(file)
-    
-    return result_files
+                # Video & Audio files 수동
+                if self.file_type == 'videos' or self.file_type == 'audios':
+                    if file.get('mimetype', 'foo/foo').split('/')[0] != self.file_type[:-1]:
+                        continue
 
-def delete_files(files):
-    count = 0
-    num_files = len(files)
-    for file in files:
-        count = count + 1
-        params = {
-            'token': token,
-            'file': file['id']
-        }
-            
-        uri = 'https://slack.com/api/files.delete'
-        response = requests.get(uri, params=params)
-        print (count, "of", num_files, "-", file['title'], json.loads(response.text)['ok'])
+                result_files.append(file)
 
-if __name__ == '__main__':
-    userId, nickname = get_my_id_nick()
+        return result_files
 
-    files = list_files(before_n_days=before_n_days, user_id=userId, file_type=file_type, exclude_starred_items=exclude_starred_items)
-    print('import', len(files), 'files')
+    def delete_files(self, files):
+        count = 0
+        num_files = len(files)
 
-    delete_files(files)
+        for file in files:
+            params = {
+                'token': self.token,
+                'file': file['id']
+            }
+
+            uri = 'https://slack.com/api/files.delete'
+            response = requests.get(uri, params=params)
+            if json.loads(response.text).get('ok', False):
+                count += 1
+
+        return num_files, count
+
+    def run(self):
+        files = self.list_files()
+        num_files, deleted_count = self.delete_files(files)
+
+        return num_files, deleted_count
