d95fb9dcb6dcf29faedef46b5b45646177687a2d
Nerrtica <nerrtica@naver.com>
2017-05-29 11:05:58 +0900

    users 정보를 dill 모듈로 save 및 load하게 수정

--- hubot/users.py
+++ hubot/users.py
@@ -1,4 +1,6 @@
 #-*- coding:utf-8 -*-
+from collections import defaultdict
+import dill
 
 
 class Users:
@@ -8,31 +10,17 @@ class Users:
     """
 
     def __init__(self, commands_file):
-        self.users = {}
+        self.users = None
         self.users_file = commands_file
         self.load()
 
     def load(self):
-        with open(self.users_file, 'r', encoding='utf-8') as f:
-            id = None
-            for line in f:
-                line = line.strip()
-                if 'id' in line:
-                    self.users[line[len('id : '):]] = {}
-                elif 'grade' in line:
-                    self.users.get(id, {})['grade'] = int(line[len('grade : '):])
-                elif 'gold' in line:
-                    self.users.get(id, {})['gold'] = int(line[len('gold : '):])
-                elif 'is_ignored' in line:
-                    self.users.get(id, {})['is_ignored'] = bool(line[len('is_ignored : '):])
-                elif 'is_admin' in line:
-                    self.users.get(id, {})['is_admin'] = bool(line[len('is_admin : '):])
+        try:
+            with open(self.users_file, 'r', encoding='utf-8') as f:
+                self.users = dill.load(f)
+        except FileNotFoundError:
+            self.users = defaultdict(lambda: {'grade': 0, 'gold': 0, 'is_ignored': False, 'is_admin': False})
 
     def save(self):
         with open(self.users_file, 'w', encoding='utf-8') as f:
-            for id, info  in self.users.items():
-                f.write('id : %s\n' % id)
-                f.write('grade : %d\n' % info.get('grade', 0))
-                f.write('gold : %d\n' % info.get('gold', 0))
-                f.write('is_ignored : %d\n' % int(info.get('is_ignored', 1)))
-                f.write('is_admin : %d\n' % int(info.get('is_admin', 0)))
+            dill.dump(self.users, f)
